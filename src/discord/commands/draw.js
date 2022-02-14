b68eb8f8388343e93d28511b93a50119dcc8681f
deadfish12 <61008844+deadfish12@users.noreply.github.com>
2022-02-14 21:26:55 +0800

    like this?

--- src/discord/commands/draw.js
+++ src/discord/commands/draw.js
@@ -1,6 +1,7 @@
 const db = require('../../Helper/db.js');
 const {serverParamsToDBName}= require('../../Helper/eventHelper.js');
 const eventMessages = require("../../../editables/messages.js");
+let {getDiscordScheduledEvents} = require("../../discord/init.js");
 
 module.exports = {
     run: async function(bot,msg,params){
@@ -17,5 +18,8 @@ module.exports = {
         finally{ 
             msg.channel.send(eventMessages.eventDraw(server,game));
         }
+        //end the nearest event
+        const events = await getDiscordScheduledEvents();
+        events.at(0).delete();
     }
 }
