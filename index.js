a2261ea35f7024916d8af5c8d64f2d01760aab38
billyovo <30436689+billyovo@users.noreply.github.com>
2022-07-07 12:49:47 +0800

    remove useless line

--- index.js
+++ index.js
@@ -49,6 +49,4 @@ bot.on('interactionCreate', async (interaction) => {
 	const { commandName } = interaction;
     bot.commands.get(commandName)?.run(bot,interaction);   
 });
-process.on('uncaughtException', function(err) {
-  console.log('Caught exception: ' + err);
-});
+
