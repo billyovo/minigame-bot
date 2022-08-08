5603808542cd0c0ef4aa6f7f10debade1c9d2265
billyovo <30436689+billyovo@users.noreply.github.com>
2022-08-08 17:22:54 +0800

    reinit

--- src/discord/slash_commands/time.js
+++
@@ -1,7 +0,0 @@
-const {getEventSchedule} = require("../../utility/checkEvents");
-module.exports = {
-    run: function(bot, interaction){
-        const selectedEvent = getEventSchedule()[interaction.options.get("name").value];
-        interaction.reply(selectedEvent.emote+" "+selectedEvent.title+" "+selectedEvent.emote+"\r\n"+`<:cobblestone:833225746020696075> 空島: <t:${parseInt(selectedEvent.date.toSeconds())}:f>`+"\r\n"+`<:grassblock:833226098020057088> 生存: <t:${parseInt(selectedEvent.date.plus({hours: 1}).toSeconds())}:f>`);
-    }
-}
