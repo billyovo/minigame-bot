b71781854d475e32889a7317d1000b9b461d2b36
billyovo <30436689+billyovo@users.noreply.github.com>
2022-08-26 14:35:33 +0800

    fix banlist

--- src/discord/slash_commands/banlist.js
+++ src/discord/slash_commands/banlist.js
@@ -4,9 +4,9 @@ const {EmbedBuilder} = require('discord.js');
 
 module.exports = {
     run: function(bot, interaction){
-        const query = `SELECT player.name, a.event FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY event ORDER BY date DESC) AS rowNumber FROM ??) a INNER JOIN player ON a.player = player.uuid WHERE rowNumber = 1`;
+            const query = `SELECT player.name, a.event FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY event ORDER BY date DESC) AS rowNumber FROM ??) a INNER JOIN player ON a.player = player.uuid WHERE rowNumber = 1`;
             const formatReply = (interaction.options.get("server").value === "survival" ? "生存" : "空島")+"服 禁賽名單";
-            const embed = new EmbedBuilder
+            const embed = new EmbedBuilder()
                         .setColor('#ee831b')
                         .setTitle(formatReply);
             db.query(query,[interaction.options.get("server").value])
